451afdad9f503e164aa238f458256e4db9a942d9
Daniela <danielaquinal@hotmail.com>
2020-09-10 12:47:31 -0600

    act5

---
+++ act_5.java
@@ -0,0 +1,39 @@
+package act_5;
+
+public class act_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 21;
+		int y = 6;
+		
+		int suma; 
+		int resta;
+		int multiplicacion;
+		int division;
+		int modulo;
+		
+		
+		System.out.println("Primer número: " + x);
+		System.out.println("Segundo número: " + y);
+		
+		
+		suma=x+y;
+		System.out.println("Suma: " + suma);
+		
+		resta=x-y;
+		System.out.println("Resta: " + resta);
+		
+		multiplicacion=x*y;
+		System.out.println("Multiplicación: " + multiplicacion);
+		
+		division=x/y;
+		System.out.println("División: " + division);
+		
+		modulo=x%y;
+		System.out.println("Módulo: " + modulo);
+		
+	}
+
+}
